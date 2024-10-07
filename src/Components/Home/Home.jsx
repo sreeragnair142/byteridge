3b0552593b3087c0262c25877184dd153a6b820f
Sreerag o s <sreeragsnair142@gmail.com>
2024-10-07 10:43:26 +0530

    initial commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="Home-text">
+        <h1>Building Excellence in Digital</h1>
+
+        <p className="cntnt md:text-lg max-w-xl">
+          We Partner with Clients to Build digital products, prioritizing
+          excellence at every stage
+        </p>
+
+        <button>Explore our services</button>
+    
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/Home/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="navbar-container sm:text-center">
+      <img 
+        src="https://byteridge.com/wp-content/uploads/2023/11/Byteridge-logo.png"
+        alt=" "
+      />
+      <ul className="navbar-menu">
+        <div className="list">
+          <li>Services</li>
+          <li>Knowledge center</li>
+          <li>Careers</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          
+          <div className="logo">
+            <img
+              src="https://byteridge.com/wp-content/uploads/2023/11/Great-place-to-work-2024-2025-badge-e1714665866122.webp "
+              alt=""
+            />
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
